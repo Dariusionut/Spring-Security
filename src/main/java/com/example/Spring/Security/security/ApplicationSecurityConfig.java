08257ebaa0ff4d7dfc91a026d4de7d07f320a32d
Darius Tinculescu <dariustinculescu@gmail.com>
2021-06-24 05:52:25 +0300

    Custom login page + thymeleaf dependency

--- src/main/java/com/example/Spring/Security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/Spring/Security/security/ApplicationSecurityConfig.java
@@ -45,7 +45,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+//                .httpBasic()
+                .formLogin()
+                .loginPage("/login").permitAll();
     }
 
     @Override
